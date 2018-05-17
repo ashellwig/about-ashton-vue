663272f6a517b79e1ac85492e018635ccbaaabdc
Ashton Hellwig <ahellwig.dev@gmail.com>
2018-05-16 23:02:44 -0600

    personal pages

--- src/views/personals/Family.vue
+++ src/views/personals/Family.vue
@@ -1,8 +1,22 @@
 <template>
   <div class="family">
-    <v-carousel>
-      <v-carousel-item v-for="(photo, i) in photos" :src="photo.src" :key="i"></v-carousel-item>
-    </v-carousel>
+    <v-container fixed>
+      <v-slide-y-transition mode="out-in">
+        <v-layout column align-center>
+          <v-card>
+            <v-card-title primary-title>
+              <div>
+                <h1>My Family</h1>
+              </div>
+            </v-card-title>
+          </v-card>
+          <br>
+          <v-carousel>
+            <v-carousel-item v-for="(photo, i) in photos" :src="photo.src" :key="i"></v-carousel-item>
+          </v-carousel>
+        </v-layout>
+      </v-slide-y-transition>
+    </v-container>
   </div>
 </template>
 
--- src/views/personals/Pets.vue
+++ src/views/personals/Pets.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="pets">
-    <h1>{{ msg }}</h1>
+    <v-container>
+      <v-slide-y-transition mode="out-in">
+        <v-layout>
+          <v-carousel>
+            <v-carousel-item v-for="(photo, i) in photos" :src="photo.src" :key="i"></v-carousel-item>
+          </v-carousel>
+        </v-layout>
+      </v-slide-y-transition>
+    </v-container>
   </div>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    msg: 'Hello'
-  })
+  data: () => {
+    return {
+      photos: [{
+        src: '/pets/ijustjoined.png'
+      }]
+    }
+  }
 }
 </script>
